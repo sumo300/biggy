d94c526d0c94bbca67d2bd7dcf1cca0fe54e1de6
xivsolutions <xivsolutions@gmail.com>
2014-03-22 13:57:11 -0500

    Remove some clutter, temporarily. DON'T MERGE THIS BRANCH.

--- Tests/Postgres/FullText.cs
+++ Tests/Postgres/FullText.cs
@@ -1,34 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Biggy.Postgres;
-using Xunit;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Biggy.Postgres;
+//using Xunit;
 
-namespace Tests.Postgres {
-  [Trait("PG Full Text Searching","")]
-  public class PGFullText {
+//namespace Tests.Postgres {
+//  [Trait("PG Full Text Searching","")]
+//  public class PGFullText {
 
-    PGList<Film> films;
-    public PGFullText() {
-      films = new PGList<Film>("chinookPG", "film");
-    }
+//    PGList<Film> films;
+//    public PGFullText() {
+//      films = new PGList<Film>("chinookPG", "film");
+//    }
 
-    [Fact(DisplayName = "Ad hoc TS Vector Query Returns Films")]
-    public void AdHocTSVector() {
+//    [Fact(DisplayName = "Ad hoc TS Vector Query Returns Films")]
+//    public void AdHocTSVector() {
 
-      var results = films.FullTextOnTheFly("monkey", "description", "title");
-      Assert.True(results.Count() > 0);
+//      var results = films.FullTextOnTheFly("monkey", "description", "title");
+//      Assert.True(results.Count() > 0);
       
-    }
+//    }
 
-    [Fact(DisplayName = "Tagged full text works wonders")]
-    public void TaggedFullText() {
+//    [Fact(DisplayName = "Tagged full text works wonders")]
+//    public void TaggedFullText() {
 
-      var results = films.FullText("monkey");
-      Assert.True(results.Count() > 0);
+//      var results = films.FullText("monkey");
+//      Assert.True(results.Count() > 0);
 
-    }
-  }
-}
+//    }
+//  }
+//}
