51e609edb1bd3b80620b2502325c022ef3093b33
xivsolutions <xivsolutions@gmail.com>
2014-05-24 14:40:05 -0500

    Modify IBiggyStore to include all CRUD methods, remove IUpdateableBiggyStore #103

--- Biggy/IBiggyStore.cs
+++ Biggy/IBiggyStore.cs
@@ -3,24 +3,18 @@ using System.Linq;
 
 namespace Biggy
 {
-    public interface IBiggyStore<T>
-    {
-        List<T> Load();
-        void SaveAll(List<T> items);
-        void Clear();     
-        T Add(T item);
-        List<T> Add(List<T> items);
-    }
+  public interface IBiggyStore<T> {
+    List<T> Load();
+    void SaveAll(List<T> items);
+    void Clear();
+    T Add(T item);
+    List<T> Add(List<T> items);
+    T Update(T item);
+    T Remove(T item);
+    List<T> Remove(List<T> items);
+  }
 
-    public interface IUpdateableBiggyStore<T> : IBiggyStore<T>
-    {
-        T Update(T item);
-        T Remove(T item);
-        List<T> Remove(List<T> items);
-    }
-
-    public interface IQueryableBiggyStore<T> : IBiggyStore<T>
-    {
-        IQueryable<T> AsQueryable();
-    }
+  public interface IQueryableBiggyStore<T> : IBiggyStore<T> {
+    IQueryable<T> AsQueryable();
+  }
 }
