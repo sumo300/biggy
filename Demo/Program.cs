c0bd66e3cbdc45ab3f2abf1f91dcfd2342e393ba
xivsolutions <xivsolutions@gmail.com>
2014-11-17 23:40:33 -0600

    Organize Demo Program file

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -5,36 +5,40 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Demo {
+
   class Program {
     static void Main(string[] args) {
 
-      pgRelationalDemo _pgRelationalDemo = new pgRelationalDemo();
+      //// POSTGRES DEMOS: +++++++++++++++++++++++++++++++++++++++++++++++++++++
+      //pgRelationalDemo _pgRelationalDemo = new pgRelationalDemo();
       //pgDocumentDemo _pgDocumentDemo = new pgDocumentDemo();
-
-      sqliteRelationalDemo _sqliteRelationalDemo = new sqliteRelationalDemo();
-      //sqliteDocumentDemo _sqliteDocumentDemo = new sqliteDocumentDemo();
-
-      //jsonPocoDemo _jsonPocoDemo = new jsonPocoDemo();
-      //jsonDocumentDemo _jsonDocumentDemo = new jsonDocumentDemo();
 
       //_pgRelationalDemo.Run();
       //Console.WriteLine("");
 
-      //_pgDocumentDemo.Run();
-      //Console.WriteLine("");
+      ////_pgDocumentDemo.Run();
+      ////Console.WriteLine("");
 
-      _sqliteRelationalDemo.Run();
-      Console.WriteLine("");
+      //// SQL LITE DEMOS: ++++++++++++++++++++++++++++++++++++++++++++++++++++++
+      //sqliteRelationalDemo _sqliteRelationalDemo = new sqliteRelationalDemo();
+      ////sqliteDocumentDemo _sqliteDocumentDemo = new sqliteDocumentDemo();
+
+      //_sqliteRelationalDemo.Run();
+      //Console.WriteLine("");
 
       //_sqliteDocumentDemo.Run();
       //Console.WriteLine("");
+
+
+      // JSON DEMOS: ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+      //jsonPocoDemo _jsonPocoDemo = new jsonPocoDemo();
+      //jsonDocumentDemo _jsonDocumentDemo = new jsonDocumentDemo();
 
       //_jsonPocoDemo.Run();
       //Console.WriteLine("");
 
       //_jsonDocumentDemo.Run();
       //Console.WriteLine("");
-
 
       Console.Read();
     }
