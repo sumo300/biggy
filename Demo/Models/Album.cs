76e1e5d18f2c64f60deeffc1e437c2fd77a9fec7
xivsolutions <xivsolutions@gmail.com>
2014-11-23 08:50:27 -0600

    Fix code styling for Album

--- Demo/Models/Album.cs
+++ Demo/Models/Album.cs
@@ -11,19 +11,17 @@ namespace Demo.Models
 {
     using System;
     using System.Collections.Generic;
-    
-    public partial class Album
-    {
-        public Album()
-        {
-            //this.Tracks = new HashSet<Track>();
-        }
-    
-        public int AlbumId { get; set; }
-        public string Title { get; set; }
-        public int ArtistId { get; set; }
-    
-        //public virtual Artist Artist { get; set; }
-        //public virtual ICollection<Track> Tracks { get; set; }
+
+  public partial class Album {
+    public Album() {
+      //this.Tracks = new HashSet<Track>();
     }
+
+    public int AlbumId { get; set; }
+    public string Title { get; set; }
+    public int ArtistId { get; set; }
+
+    //public virtual Artist Artist { get; set; }
+    //public virtual ICollection<Track> Tracks { get; set; }
+  }
 }
