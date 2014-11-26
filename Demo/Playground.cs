d1e355e607b7d047e6c77a6e6404fae53ab543e0
xivsolutions <xivsolutions@gmail.com>
2014-11-26 15:25:56 -0600

    Add playground class to Demo project for spikes/misc trial stuff.

---
+++ Demo/Playground.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Biggy.Core;
+using Biggy.Data.Json;
+using Demo.Models;
+using System.IO;
+using System.Security.Cryptography;
+using Biggy.Extensions;
+
+namespace Demo {
+  public class Playground {
+
+    //JsonDbCore _db = new JsonDbCore("Playground");
+
+
+    public void Run() {
+
+      //var artistStore = new JsonStore<Artist>(new JsonDbCore("Chinook"));
+      //var memoryArtists = new BiggyList<Artist>();
+      //memoryArtists.Add(artistStore.TryLoadData());
+
+      //var single = memoryArtists.FirstOrDefault();
+      //single.Name = "Updated";
+      //memoryArtists.Update(single);
+
+      //memoryArtists.Clear();
+
+      //memoryArtists.ItemAdded += memoryArtists_ItemAdded;
+      //memoryArtists.Add(new Artist { ArtistId = 500, Name = "My Added Artist" });
+      var _db = new JsonDbCore("workUnit");
+      var artistList = new BiggyList<Artist>(_db.CreateStoreFor<Artist>());
+      var syncDictionary = new Dictionary<Artist, IDictionary<string, object>>();
+      for (int i = 1; i <= 10; i++) {
+        var artist = new Artist { ArtistId = i, Name = "Artist " + i };
+        artistList.Add(artist);
+      }
+
+      var workUnit = new BiggyWorkUnit<Artist>(artistList);
+      workUnit.Add(new Artist { ArtistId = 100, Name = "AddedSingle" });
+      var updateMe = workUnit.FirstOrDefault();
+      updateMe.Name = "Updated";
+      workUnit.Update(updateMe);
+      workUnit.SubmitChanges();
+      
+    }
+
+
+
+
+
+    void memoryArtists_ItemAdded(object sender, BiggyEventArgs<Artist> e) {
+      Console.WriteLine(e.Item.Name);
+    }
+  }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -5,26 +5,29 @@ namespace Demo {
   internal class Program {
     private static void Main(string[] args) {
       //// POSTGRES DEMOS: +++++++++++++++++++++++++++++++++++++++++++++++++++++
+
       //pgRelationalDemo _pgRelationalDemo = new pgRelationalDemo();
       //pgDocumentDemo _pgDocumentDemo = new pgDocumentDemo();
 
       //_pgRelationalDemo.Run();
       //Console.WriteLine("");
 
-      ////_pgDocumentDemo.Run();
-      ////Console.WriteLine("");
+      //_pgDocumentDemo.Run();
+      //Console.WriteLine("");
 
       //// SQL LITE DEMOS: ++++++++++++++++++++++++++++++++++++++++++++++++++++++
-      sqliteRelationalDemo _sqliteRelationalDemo = new sqliteRelationalDemo();
-      ////sqliteDocumentDemo _sqliteDocumentDemo = new sqliteDocumentDemo();
 
-      _sqliteRelationalDemo.Run();
+      //sqliteRelationalDemo _sqliteRelationalDemo = new sqliteRelationalDemo();
+      //sqliteDocumentDemo _sqliteDocumentDemo = new sqliteDocumentDemo();
+
+      //_sqliteRelationalDemo.Run();
       //Console.WriteLine("");
 
       //_sqliteDocumentDemo.Run();
       //Console.WriteLine("");
 
       // JSON DEMOS: ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
       //jsonPocoDemo _jsonPocoDemo = new jsonPocoDemo();
       //jsonDocumentDemo _jsonDocumentDemo = new jsonDocumentDemo();
 
@@ -33,6 +36,11 @@ namespace Demo {
 
       //_jsonDocumentDemo.Run();
       //Console.WriteLine("");
+
+      // Playground/Spike: ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+      Playground _playground = new Playground();
+      _playground.Run();
 
       Console.Read();
     }
